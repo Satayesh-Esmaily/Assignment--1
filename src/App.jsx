ab0e8e87b010684ab19f8ea61bcb0835c03cb679
Satayesh Esmaily <setayeshesmaily59@gmail.com>
2026-02-07 15:48:44 +0430

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,20 @@
 import "./App.css";
+import Badge from "./components/Badge";
 import TodoList from "./components/TodoList";
 
 export default function App() {
-  const tasks = []; 
-
   return (
-    <div className="app"> {}
-      <TodoList tasks={tasks} />
+    <div className="app">
+      {/* Week 1 Assignment - reusable Badge component */}
+      <div className="badges">
+        <Badge label="Course" value="React" />
+        <Badge label="Week" value="1" />
+        <Badge label="Topic" value="Props" />
+        <Badge label="Student" value="Satayesh" />
+      </div>
+
+      {/* Existing todo list app */}
+      <TodoList />
     </div>
   );
 }
